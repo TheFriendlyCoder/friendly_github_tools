40b2142541f281a44e0d68715c1377d57ab29578
Kevin Phillips <thefriendlycoder@gmail.com>
2021-09-06 19:49:22 -0300

    Made code synchronous

--- friendly_github_tools/Program.cs
+++ friendly_github_tools/Program.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 // Reference link: https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
 namespace friendly_github_tools
@@ -16,39 +15,43 @@ namespace friendly_github_tools
         public string Name { get; set; }
     }
 
-    class Program
+    class Github
     {
         public static readonly HttpClient client = new HttpClient();
 
-        private static async Task ProcessRepositories()
+        public List<Repository> GetRepos()
         {
             var token = "";
             if (token != "")
             {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(token);
             }
-            
+
 
             //client.DefaultRequestHeaders.Accept.Clear();
             //client.DefaultRequestHeaders.Accept.Add(
             //    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
-            //var msg = await stringTask;
-            //Console.Write(msg);
-            
             var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
-            foreach (var repo in repositories)
-                Console.WriteLine(repo.Name);
-
+            
+            return JsonSerializer.DeserializeAsync<List<Repository>>(streamTask.Result).Result;
         }
-        static async Task Main(string[] args)
-        //static void Main(string[] args)
-        {
 
-            await ProcessRepositories();
+    }
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var gh = new Github();
+            var repositories = gh.GetRepos();
+            foreach (var i in repositories)
+            {
+                Console.WriteLine(i.Name);
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
